50a23b13fa0e43fe7b9281efa2868e673ccdd9ee
Jarnen Richard <richard.jarnen@gmail.com>
2020-08-13 22:47:08 +0800

    refactor: added about and contact templates

--- taskmate/todolist/urls.py
+++ taskmate/todolist/urls.py
@@ -3,4 +3,6 @@ from todolist import views
 
 urlpatterns = [
     path('', views.todolist, name='todolist'),
+    path('contact', views.contact, name='contact'),
+    path('about', views.about, name='about'),
 ]
--- taskmate/todolist/views.py
+++ taskmate/todolist/views.py
@@ -5,6 +5,20 @@ from django.http import HttpResponse
 
 def todolist(request):
     context = {
-        'welcome_text': "Welcome from Jinja2",
+        'welcome_text': "Welcome to Todolist",
     }
     return render(request, 'todolist.html', context)
+
+
+def contact(request):
+    context = {
+        'welcome_text': "Welcome to Contacts Page",
+    }
+    return render(request, 'contacts.html', context)
+
+
+def about(request):
+    context = {
+        'welcome_text': "Welcome from About Page",
+    }
+    return render(request, 'about.html', context)
