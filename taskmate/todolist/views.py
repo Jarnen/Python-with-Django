6cda45449b2738fdfd5295248b8b29551591dcf9
Jarnen Richard <richard.jarnen@gmail.com>
2020-08-22 09:35:56 +0800

    refactor: Migrate and Register Model Migrate and Register models and add to the view. Updated todolist.html to view.

--- taskmate/todolist/views.py
+++ taskmate/todolist/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+#to fetch data from DB
+# First step import the model
+from todolist.models import TaskList
+
 # Create your views here.
 
 
 def todolist(request):
-    context = {
-        'welcome_text': "Welcome to Todolist",
-    }
-    return render(request, 'todolist.html', context)
+    # Fetch all the objects from DB
+    all_tasks = TaskList.objects.all
+
+    return render(request, 'todolist.html', {'all_tasks': all_tasks})
+    # context = {
+    #     'welcome_text': "Welcome to Todolist",
+    # }
+    # return render(request, 'todolist.html', context)
 
 
 def contact(request):
