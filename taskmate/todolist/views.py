d670e2a6d416d7b90d4e5c0865aa3840fc510906
Jarnen Richard <richard.jarnen@gmail.com>
2020-08-24 15:31:45 +0800

    refactor: add form and accept input

--- taskmate/todolist/views.py
+++ taskmate/todolist/views.py
@@ -1,22 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
-#to fetch data from DB
-# First step import the model
 from todolist.models import TaskList
+from todolist.forms import TaskForm
 
 # Create your views here.
-
-
 def todolist(request):
-    # Fetch all the objects from DB
-    all_tasks = TaskList.objects.all
+    if request.method == "POST":
+        form = TaskForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        return redirect('todolist')
+    else:
+        # Fetch all the objects from DB
+        all_tasks = TaskList.objects.all
 
-    return render(request, 'todolist.html', {'all_tasks': all_tasks})
-    # context = {
-    #     'welcome_text': "Welcome to Todolist",
-    # }
-    # return render(request, 'todolist.html', context)
+        return render(request, 'todolist.html', {'all_tasks': all_tasks})
+        # context = {
+        #     'welcome_text': "Welcome to Todolist",
+        # }
+        # return render(request, 'todolist.html', context)
 
 
 def contact(request):
