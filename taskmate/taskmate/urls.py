e923c18d04bd67dfad4ef23e4b7d4af970f3f75d
Jarnen Richard <richard.jarnen@gmail.com>
2020-09-11 09:41:47 +0800

    refactor: updated contact and about urls

--- taskmate/taskmate/urls.py
+++ taskmate/taskmate/urls.py
@@ -1,8 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from todolist import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todolist/', include('todolist.urls'))
+    path('todolist/', include('todolist.urls')),
+    path('contact', views.contact, name='contact'),
+    path('about', views.about, name='about'),
 ]
