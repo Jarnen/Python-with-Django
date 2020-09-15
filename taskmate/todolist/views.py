d219e532e3c6c3450348781b66131b35e662e926
Jarnen Richard <richard.jarnen@gmail.com>
2020-09-15 19:10:54 +0800

    refactor: add login user restrictions

--- taskmate/todolist/views.py
+++ taskmate/todolist/views.py
@@ -4,8 +4,10 @@ from todolist.models import TaskList
 from todolist.forms import TaskForm
 from django.contrib import messages
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def todolist(request):
     if request.method == "POST":
         form = TaskForm(request.POST or None)
@@ -41,24 +43,27 @@ def about(request):
     }
     return render(request, 'about.html', context)
 
+@login_required
 def delete_task(request, task_id):
     task = TaskList.objects.get(pk=task_id)
     task.delete()
     return redirect('todolist')
 
+@login_required
 def complete_task(request, task_id):
     task = TaskList.objects.get(pk=task_id)
     task.done = True
     task.save()
     return redirect('todolist')
 
+@login_required
 def pending_task(request, task_id):
     task = TaskList.objects.get(pk=task_id)
     task.done = False
     task.save()
     return redirect('todolist')
 
-
+@login_required
 def edit_task(request, task_id):
      if request.method == "POST":
          task = TaskList.objects.get(pk=task_id)
